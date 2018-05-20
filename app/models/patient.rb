b0bf06852db8a7332a1351132b46134cabbb7b31
Kim Foukaris <kimfoukaris@outlook.com>
2018-05-20 04:08:25 +0000

    automatically backed up by learn

--- app/models/patient.rb
+++ app/models/patient.rb
@@ -1,13 +1,5 @@
 class Patient < ActiveRecord::Base
   has_many :appointments
   has_many :doctors, through: :appointments
-  accepts_nested_attributes_for :doctors
-
-  def doctor_id=(id)
-    self.doctor = Doctor.find(id)
-  end
-
-  def doctor_id
-    self.doctor.id
-  end
+  
 end
